1539c530fc16bd979b95a1b3e17f94f879a19e5d
svend <semt1992@gmail.com>
2023-11-11 21:47:52 +0800

    local api test works

--- studentGradesFront/src/app/app.module.ts
+++ studentGradesFront/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StudentDataComponent } from './student-data/student-data.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { StudentDataComponent } from './student-data/student-data.component';
     StudentDataComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- studentGradesFront/src/app/student-data/student-data.component.ts
+++ studentGradesFront/src/app/student-data/student-data.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-student-data',
   templateUrl: './student-data.component.html',
   styleUrls: ['./student-data.component.scss']
 })
-export class StudentDataComponent {
+export class StudentDataComponent implements OnInit {
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getClassNames();
+  }
+
+  getClassNames() {
+    this.http.get<any>('http://127.0.0.1:8000/studentgradesapp/classnames/').subscribe(
+      data => {
+        console.log(data);
+      },
+      error => {
+        console.error('There was an error!', error);
+      }
+    );
+
+  }
 }
